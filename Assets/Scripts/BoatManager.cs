00598ea167e317d78a8f7c885d89f98e317e7d92
NicoJanssonPersonal <janssnic@arcada.fi>
2026-07-18 15:46:54 +0300

    death

--- Assets/Scripts/BoatManager.cs
+++ Assets/Scripts/BoatManager.cs
@@ -1,10 +1,21 @@
+using System.Collections;
+using Unity.VectorGraphics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BoatManager : MonoBehaviour
 {
     public GameObject fishradius;
     private float fishrangeRatio = 1.4f;
 
+    private Rigidbody rb;
+    public Collider boatCollider;
+    private bool isSinking = false;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     void Start()
     {
         
@@ -13,10 +24,50 @@ public class BoatManager : MonoBehaviour
     void Update()
     {
         updateFishingRange();
+        checkHp();
     }
     void updateFishingRange()
     {
         float fishRange = GlobalStats.fishingRange * fishrangeRatio;
         fishradius.transform.localScale = new Vector3(fishRange, 1, fishRange);
+    }
+    void checkHp()
+    {
+        if (GlobalStats.currentHealth <= 0)
+        {
+            Debug.Log("game OVER");
+            StartSinking();
+            fishradius.SetActive(false);
+        }
+        if(transform.position.y <= -10)
+        {
+            //Time.timeScale = 0;
+            //pause game popup window u died return to tavern
+            StartCoroutine(changeScene("TAVERN"));
+        }
+    }
+    public void StartSinking()
+    {
+        isSinking = true;
+        
+        if (boatCollider!= null)
+        {
+            boatCollider.enabled = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isSinking)
+        {
+            Vector3 downwardMovement = Vector3.down * 2 * Time.fixedDeltaTime;
+            rb.MovePosition(rb.position + downwardMovement);
+        }
+    }
+    private IEnumerator changeScene(string scene)
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(scene);
+
     }
 }
