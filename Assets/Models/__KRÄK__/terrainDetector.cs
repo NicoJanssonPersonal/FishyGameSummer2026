ad04e118b339427fcd7262d97385359e7b364bb4
nico <janssnic@arcada.fi>
2026-08-10 17:16:00 +0300

    shark terrain avoidance system STAS

---
+++ Assets/Models/__KRÄK__/terrainDetector.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class TerrainDetector : MonoBehaviour
+{
+    public Transform sharkParent; 
+    
+    public LayerMask terrainLayer;
+    
+    public float avoidanceSpeed = 4f;
+
+    void Start()
+    {
+        if (sharkParent == null) sharkParent = transform.parent;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & terrainLayer) != 0)
+        {
+            Vector3 closestPoint = other.ClosestPoint(transform.position);
+
+            Vector3 avoidDirection = transform.position - closestPoint;
+
+            if (avoidDirection == Vector3.zero) 
+                avoidDirection = sharkParent.up; 
+
+            Quaternion targetRotation = Quaternion.LookRotation(avoidDirection.normalized);
+            sharkParent.rotation = Quaternion.Slerp(
+                sharkParent.rotation, 
+                targetRotation, 
+                Time.deltaTime * avoidanceSpeed
+            );
+        }
+    }
+}
